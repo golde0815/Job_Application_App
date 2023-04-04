875b51706bf88a8a430ecbdb4e145c12fa241990
dannsy <danielngsy@pm.me>
2023-04-03 21:24:12 -0700

    Cleanup import statements

--- backend/src/main/java/ca/ubc/cs304/controller/PostedJobController.java
+++ backend/src/main/java/ca/ubc/cs304/controller/PostedJobController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
--- backend/src/main/java/ca/ubc/cs304/database/dao/PostedJobDao.java
+++ backend/src/main/java/ca/ubc/cs304/database/dao/PostedJobDao.java
@@ -15,12 +15,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 @Component
 public class PostedJobDao {
